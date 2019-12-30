3c61d4fcc770ed696d393aa2ab13abd4ac68a236
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2019-12-31 01:50:04 +0530

    v 0.1

--- src/PDA.php
+++ src/PDA.php
@@ -3,4 +3,13 @@ namespace PDA;
 
 class PDA
 {
+    public function insert(string $table, array $columns, array $values) :string
+    {
+        $return = 'error';
+        if ($table === '' || !(count($columns) === count($values[0]))) {
+            return $return;
+        }
+        $return = 'success';
+        return $return;
+    }
 }
