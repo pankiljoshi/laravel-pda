e129fa9daa1ec4ae227a385fc5e586da2d7bb37a
Jagdeep Singh <jagdeepsingh@ucreate.co.in>
2020-01-03 10:07:39 +0530

    WIP for DynamoDB DB interactions

--- src/PDA.php
+++ src/PDA.php
@@ -1,8 +1,98 @@
 <?php
 namespace PDA;
 
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Aws\DynamoDb;
+use Aws\Sdk;
+
 class PDA
 {
+    public function createTable(): void
+    {
+        $sdk = new Sdk([
+                           'endpoint'   => 'http://localhost:8000',
+                           'region'   => 'ap-south-1',
+                           'version'  => 'latest'
+                       ]);
+        $dynamodb = $sdk->createDynamoDb();
+
+        $categories = [
+            'TableName' => 'categories',
+            'KeySchema' => [
+                [
+                    'AttributeName' => 'id',
+                    'KeyType' => 'HASH'
+                ],
+                [
+                    'AttributeName' => 'name',
+                    'KeyType' => 'RANGE'
+                ],
+            ],
+            'AttributeDefinitions' => [
+                [
+                    'AttributeName' => 'id',
+                    'AttributeType' => 'N'
+                ],
+                [
+                    'AttributeName' => 'name',
+                    'AttributeType' => 'S'
+                ],
+                [
+                    'AttributeName' => 'active',
+                    'AttributeType' => 'B'
+                ],
+            ]
+        ];
+        $products = [
+            'TableName' => 'products',
+            'KeySchema' => [
+                [
+                    'AttributeName' => 'id',
+                    'KeyType' => 'HASH'
+                ],
+                [
+                    'AttributeName' => 'category_id',
+                    'KeyType' => 'RANGE'
+                ],
+            ],
+            'AttributeDefinitions' => [
+                [
+                    'AttributeName' => 'id',
+                    'AttributeType' => 'N'
+                ],
+                [
+                    'AttributeName' => 'category_id',
+                    'AttributeType' => 'N'
+                ],
+                [
+                    'AttributeName' => 'name',
+                    'AttributeType' => 'S'
+                ],
+                [
+                    'AttributeName' => 'cost',
+                    'AttributeType' => 'S'
+                ],
+                [
+                    'AttributeName' => 'color',
+                    'AttributeType' => 'S'
+                ],
+            ]
+        ];
+
+        try {
+            $result = $dynamodb->createTable($categories);
+            echo 'Created table.  Status: ' .
+                $result['TableDescription']['TableStatus'] ."\n";
+            $result = $dynamodb->createTable($products);
+            echo 'Created table.  Status: ' .
+                $result['TableDescription']['TableStatus'] ."\n";
+
+        } catch (DynamoDbException $e) {
+            echo "Unable to create table:\n";
+            echo $e->getMessage() . "\n";
+        }
+    }
     public function insert(string $table, array $columns, array $values) :string
     {
         $return = 'error';
